14757ec237d5942f81f4f3881edb002529bd4de1
Kuba <jakub.smolka@4chain.studio>
2023-08-30 18:22:59 +0200

    chore(BUX-131): changing the API reponse type for merkle roots verification

--- transports/http/endpoints/api/merkleroots/model.go
+++ transports/http/endpoints/api/merkleroots/model.go
@@ -4,20 +4,27 @@ import (
 	"github.com/libsv/bitcoin-hc/domains"
 )
 
-// merkleRootConfirmationRespose is an API response for
-// confirmation of merkle roots inclusion in the longest chain.
-type merkleRootConfirmationRespose struct {
+// merkleRootConfirmationResponse is a confirmation
+// of merkle roots inclusion in the longest chain.
+type merkleRootConfirmation struct {
 	Hash       string `json:"blockhash"`
 	MerkleRoot string `json:"merkleRoot"`
 	Confirmed  bool   `json:"confirmed"`
 }
 
-// newMerkleRootConfirmationReponse creates a new merkleRootConfirmationRespose
+// merkleRootsConfirmationsResponse is an API reponse for confirming
+// merkle roots inclusion in the longest chain.
+type merkleRootsConfirmationsResponse struct {
+	AllIncluded   bool                     `json:"allIncluded"`
+	Confirmations []merkleRootConfirmation `json:"confirmations"`
+}
+
+// newMerkleRootConfirmationcreates a new merkleRootConfirmation
 // object from domain's MerkleRootConfirmation object.
-func newMerkleRootConfirmationReponse(
+func newMerkleRootConfirmation(
 	merkleConfms *domains.MerkleRootConfirmation,
-) merkleRootConfirmationRespose {
-	return merkleRootConfirmationRespose{
+) merkleRootConfirmation {
+	return merkleRootConfirmation{
 		Hash:       merkleConfms.Hash,
 		MerkleRoot: merkleConfms.MerkleRoot,
 		Confirmed:  merkleConfms.Confirmed,
@@ -28,15 +35,20 @@ func newMerkleRootConfirmationReponse(
 // MerkleRootConfirmation objects to merkleRootConfirmationRespose.
 func mapToMerkleRootsConfirmationsResponses(
 	merkleConfms []*domains.MerkleRootConfirmation,
-) []merkleRootConfirmationRespose {
-	merkleConfmsResps := make([]merkleRootConfirmationRespose, 0)
+) merkleRootsConfirmationsResponse {
+	mrcfs := make([]merkleRootConfirmation, 0)
+
+	allIncluded := true
 
 	for _, merkleConfm := range merkleConfms {
-		merkleConfmsResps = append(
-			merkleConfmsResps,
-			newMerkleRootConfirmationReponse(merkleConfm),
-		)
+		mrcfs = append(mrcfs, newMerkleRootConfirmation(merkleConfm))
+		if !merkleConfm.Confirmed {
+			allIncluded = false
+		}
 	}
 
-	return merkleConfmsResps
+	return merkleRootsConfirmationsResponse{
+		AllIncluded:   allIncluded,
+		Confirmations: mrcfs,
+	}
 }
