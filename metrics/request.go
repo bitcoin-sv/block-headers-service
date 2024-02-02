10f80ca915b85b3a34f49fcbc8241e660aca7a73
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-02-02 14:46:51 +0100

    feat(BUX-289): metrics initialization

---
+++ metrics/request.go
@@ -0,0 +1,75 @@
+package metrics
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type RequestMetrics struct {
+	requestsTotal   *prometheus.CounterVec
+	requestDuration *prometheus.HistogramVec
+}
+
+func registerRequestMetrics(reg *prometheus.Registry) *RequestMetrics {
+	requestsTotal := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: fmt.Sprintf("%s_http_requests_total", serviceName),
+			Help: "Count of all HTTP requests",
+		},
+		[]string{"method", "path", "status", "classification"},
+	)
+	reg.MustRegister(requestsTotal)
+
+	requestDuration := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    fmt.Sprintf("%s_http_request_duration_seconds", serviceName),
+			Help:    "Duration of all HTTP requests",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "path"},
+	)
+	reg.MustRegister(requestDuration)
+
+	return &RequestMetrics{
+		requestsTotal:   requestsTotal,
+		requestDuration: requestDuration,
+	}
+}
+
+func (m *RequestMetrics) Track(method, path string) *RequestTracker {
+	return &RequestTracker{
+		method:  method,
+		path:    path,
+		metrics: m,
+	}
+}
+
+type RequestTracker struct {
+	method    string
+	path      string
+	startTime time.Time
+	metrics   *RequestMetrics
+}
+
+func (r *RequestTracker) Start() {
+	r.startTime = time.Now()
+}
+
+func (r *RequestTracker) End(status int) {
+	if status == 404 {
+		// This is a safeguard against attacks where the server is flooded with requests having unique paths,
+		// which would lead to the creation of a large number of metrics
+		r.path = "UNKNOWN_PATH"
+	}
+	r.metrics.requestsTotal.WithLabelValues(r.method, r.path, fmt.Sprint(status), requestClassification(status)).Inc()
+	r.metrics.requestDuration.WithLabelValues(r.method, r.path).Observe(time.Since(r.startTime).Seconds())
+}
+
+func requestClassification(status int) string {
+	if status >= 200 && status < 400 {
+		return "success"
+	}
+	return "failure"
+}
