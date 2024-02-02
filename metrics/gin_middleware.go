39fa0b6733af83a4c1618affbf6b8f901674f256
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-02-02 14:46:51 +0100

    refactor(BUX-289): requestMetricsMiddleware

--- metrics/gin_middleware.go
+++ metrics/gin_middleware.go
@@ -9,7 +9,11 @@ func requestMetricsMiddleware() gin.HandlerFunc {
 		return func(c *gin.Context) {
 			tracker := metrics.httpRequests.Track(c.Request.Method, c.Request.URL.Path)
 			tracker.Start()
-			defer tracker.End(c.Writer.Status()) //note that the status code will be correct only if another middleware doesn't change the status code; order of middlewares matters
+			defer func() {
+				// note that the status code will be correct only if another middleware doesn't change the status code;
+				// order of middlewares matters
+				tracker.End(c.Writer.Status())
+			}()
 
 			c.Next()
 		}
