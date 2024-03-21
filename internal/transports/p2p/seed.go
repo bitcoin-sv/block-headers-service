d3003cd4d7048725a3f32a34245365a7050fe431
kuba-4chain <jakub.smolka@4chain.studio>
2024-03-21 22:02:09 +0800

    feat(p2pexp): connect to peer and try to construct a version message

--- internal/transports/p2p/seed.go
+++ internal/transports/p2p/seed.go
@@ -1,7 +1,9 @@
 package p2pexp
 
 import (
+	"fmt"
 	"net"
+	"strings"
 
 	"github.com/bitcoin-sv/block-headers-service/internal/chaincfg"
 	"github.com/rs/zerolog"
@@ -12,7 +14,7 @@ func SeedFromDNS(seeds []chaincfg.DNSSeed, log *zerolog.Logger) []net.IP {
 
 	for _, seed := range seeds {
 		host := seed.Host
-		seedpeers, err := net.LookupIP(host)
+		seedpeers, err := bsvdLookup(host)
 		if err != nil {
 			log.Info().Msgf("DNS discovery failed on seed %s: %v", host, err)
 			continue
@@ -22,4 +24,12 @@ func SeedFromDNS(seeds []chaincfg.DNSSeed, log *zerolog.Logger) []net.IP {
 	}
 
 	return allSeedPeers
+}
+
+func bsvdLookup(host string) ([]net.IP, error) {
+	if strings.HasSuffix(host, ".onion") {
+		return nil, fmt.Errorf("attempt to resolve tor address: %s", host)
+	}
+
+	return net.LookupIP(host)
 }
