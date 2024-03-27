d605e172508268d8790476cb746adc8425dd9326
wregulski <wojciechregulski@outlook.com>
2024-03-27 09:32:38 +0100

    fix(BUX-572): remove unnecessary sleep while starting testcontainer

--- regressiontests/regression_test.go
+++ regressiontests/regression_test.go
@@ -192,8 +192,6 @@ func startPostgresContainer(ctx context.Context, t *testing.T) (testcontainers.C
 		t.Fatalf("Could not start postgres container: %v", err)
 	}
 
-	time.Sleep(10 * time.Second) // Wait for the container to be ready
-
 	mappedPort, err := postgresContainer.MappedPort(ctx, "5432")
 	if err != nil {
 		t.Fatalf("Failed to get mapped port: %v", err)
