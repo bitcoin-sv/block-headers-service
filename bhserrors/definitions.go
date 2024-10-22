b4f3c49e954211a4134f3523589fd5f3f209f969
Damian-4chain <damian.zoltowski@4chain.studio>
2024-10-22 13:35:12 +0200

    feat(SPV-1106): rm merkleroots from batchsize err

--- bhserrors/definitions.go
+++ bhserrors/definitions.go
@@ -8,5 +8,5 @@ var ErrMerklerootNotFound = BHSError{Message: "No block with provided merkleroot
 // ErrMerklerootNotInLongestChain is when provided merkleroot from user was found in Block Header Service's database but is not in Longest Chain state
 var ErrMerklerootNotInLongestChain = BHSError{Message: "Provided merkleroot is not part of the longest chain", Code: "ErrMerkleRootNotInLongestChain", StatusCode: 409}
 
-// ErrMerklerootInvalidBatchSize is when user provided incorrect batchSize
-var ErrMerklerootInvalidBatchSize = BHSError{Message: "batchSize must be 0 or a positive integer", Code: "ErrInvalidBatchSize", StatusCode: 400}
+// ErrInvalidBatchSize is when user provided incorrect batchSize
+var ErrInvalidBatchSize = BHSError{Message: "batchSize must be 0 or a positive integer", Code: "ErrInvalidBatchSize", StatusCode: 400}
