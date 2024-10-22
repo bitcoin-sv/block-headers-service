6a42ebe103aa184c53bb67d7b7fae5aeadf6dbbb
Damian-4chain <damian.zoltowski@4chain.studio>
2024-10-22 13:30:22 +0200

    feat(SPV-1106): happy linter

--- bhserrors/definitions.go
+++ bhserrors/definitions.go
@@ -2,6 +2,11 @@ package bhserrors
 
 // ////////////////////////////////// MERKLE ROOTS ERRORS
 
+// ErrMerklerootNotFound is when provided merkleroot from user was not found in Block Header Service's database
 var ErrMerklerootNotFound = BHSError{Message: "No block with provided merkleroot was found", Code: "ErrMerkleRootNotFound", StatusCode: 404}
+
+// ErrMerklerootNotInLongestChain is when provided merkleroot from user was found in Block Header Service's database but is not in Longest Chain state
 var ErrMerklerootNotInLongestChain = BHSError{Message: "Provided merkleroot is not part of the longest chain", Code: "ErrMerkleRootNotInLongestChain", StatusCode: 409}
+
+// ErrMerklerootInvalidBatchSize is when user provided incorrect batchSize
 var ErrMerklerootInvalidBatchSize = BHSError{Message: "batchSize must be 0 or a positive integer", Code: "ErrInvalidBatchSize", StatusCode: 400}
