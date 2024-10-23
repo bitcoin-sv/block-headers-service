5730152f7ffc2a3d13087e9d8345a187b7ab17f9
Damian-4chain <damian.zoltowski@4chain.studio>
2024-10-23 15:14:22 +0200

    refactor(SPV-1106): add access endpoint errors

--- bhserrors/definitions.go
+++ bhserrors/definitions.go
@@ -1,5 +1,24 @@
 package bhserrors
 
+// ////////////////////////////////// AUTH ERRORS
+
+// ErrGeneric is a generic error that something went wrong
+var ErrGeneric = BHSError{Message: "Something went wrong. Internal server error", StatusCode: 500, Code: "ErrGeneric"}
+
+// ////////////////////////////////// AUTH ERRORS
+
+// ErrMissingAuthHeader is when request does not have auth header
+var ErrMissingAuthHeader = BHSError{Message: "Empty auth header", StatusCode: 401, Code: "ErrMissingAuthHeader"}
+
+// ErrInvalidAuthHeader is when request does not have a valid auth header
+var ErrInvalidAuthHeader = BHSError{Message: "Invalid auth header", StatusCode: 401, Code: "ErrInvalidAuthHeader"}
+
+// ErrInvalidAccessToken is when access token is invalid
+var ErrInvalidAccessToken = BHSError{Message: "Invalid access token", StatusCode: 401, Code: "ErrInvalidAccessToken"}
+
+// ErrUnauthorized is a generic error when user is unauthorized to make a request
+var ErrUnauthorized = BHSError{Message: "Not authorized", StatusCode: 401, Code: "ErrUnauthorized"}
+
 // ////////////////////////////////// MERKLE ROOTS ERRORS
 
 // ErrMerklerootNotFound is when provided merkleroot from user was not found in Block Header Service's database
@@ -10,3 +29,17 @@ var ErrMerklerootNotInLongestChain = BHSError{Message: "Provided merkleroot is n
 
 // ErrInvalidBatchSize is when user provided incorrect batchSize
 var ErrInvalidBatchSize = BHSError{Message: "batchSize must be 0 or a positive integer", Code: "ErrInvalidBatchSize", StatusCode: 400}
+
+// ////////////////////////////////// TOKEN ERRORS
+
+// ErrAdminTokenNotFound is when admin token was not found in Block Header Service
+var ErrAdminTokenNotFound = BHSError{Message: "Admin token not found", StatusCode: 401, Code: "ErrAdminTokenNotFound"}
+
+// ErrTokenNotFound is when token was not found in Block Header Service
+var ErrTokenNotFound = BHSError{Message: "Token not found", StatusCode: 404, Code: "ErrTokenNotFound"}
+
+// ErrCreateToken is when create token fails
+var ErrCreateToken = BHSError{Message: "Failed to create new token", StatusCode: 400, Code: "ErrCreateToken"}
+
+// ErrDeleteToken is when delete token fails
+var ErrDeleteToken = BHSError{Message: "Failed to delete token", StatusCode: 400, Code: "ErrDeleteToken"}
--- transports/http/auth/require_auth.go
+++ transports/http/auth/require_auth.go
@@ -1,9 +1,7 @@
 package auth
 
 import (
-	"errors"
-	"net/http"
-
+	"github.com/bitcoin-sv/block-headers-service/bhserrors"
 	"github.com/bitcoin-sv/block-headers-service/domains"
 	"github.com/gin-gonic/gin"
 )
@@ -17,7 +15,7 @@ func RequireAdmin(handler gin.HandlerFunc, requireAdmin bool) gin.HandlerFunc {
 			if err := validateToken(c); err == nil {
 				handler(c)
 			} else {
-				c.AbortWithStatusJSON(http.StatusUnauthorized, err.Error())
+				bhserrors.AbortWithErrorResponse(c, err, nil)
 			}
 		}
 	}
@@ -27,14 +25,14 @@ func RequireAdmin(handler gin.HandlerFunc, requireAdmin bool) gin.HandlerFunc {
 func validateToken(c *gin.Context) error {
 	token, exist := c.Get("token")
 	if !exist {
-		return errors.New("token not found")
+		return bhserrors.ErrAdminTokenNotFound
 	}
 	t, ok := token.(*domains.Token)
 	if !ok {
-		return errors.New("something went wrong")
+		return bhserrors.ErrGeneric
 	}
 	if !t.IsAdmin {
-		return errors.New("not authorized")
+		return bhserrors.ErrUnauthorized
 	}
 	return nil // the token is valid
 }
